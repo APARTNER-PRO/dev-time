ee6301667224443b7aa9a079758b9a7ac714ed45
Roman Matviy <roman@matviy.pp.ua>
2026-03-18 15:35:19 +0200

    change domain

--- xmlrpc.php
+++ xmlrpc.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://dev-time.orh</homePageLink>
+		<homePageLink>https://devtime.apartner.pro</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://dev-time.orh/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://dev-time.orh/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://dev-time.orh/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://dev-time.orh/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://dev-time.orh/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://devtime.apartner.pro/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://devtime.apartner.pro/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://devtime.apartner.pro/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://devtime.apartner.pro/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://devtime.apartner.pro/wp-json/" />
 			</apis>
 	</service>
 </rsd>
